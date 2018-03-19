0a62f02b492e3e2c83528b58703627900cb16e27
Pawel Zawadzki <pawel@testo.systems>
2018-03-19 20:29:35 +0100

    Local api url removal. Production build flag addition.

--- src/constants.js
+++ src/constants.js
@@ -6,5 +6,5 @@ export const COMPONENTS = {
 };
 
 export const API_URL = 'https://mapstatisticsapi.azurewebsites.net/api/';
-// export const API_URL = 'http://localhost:57787/api/';
+
 export const GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places';
